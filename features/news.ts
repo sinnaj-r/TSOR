2dc57fa6583a807f25554d66d982efbcfba32f03
Jannis Rosenbaum <jannis.rosenbaum@student.hpi.de>
2021-03-23 17:00:31 +0100

    add news to store

---
+++ features/news.ts
@@ -0,0 +1,40 @@
+import { createApiSlice } from '../higherOrderReducers/HOOdataReducer';
+
+export type NewsItem = {
+  id: string;
+  title: string;
+  url: string;
+  description?: string;
+};
+
+const {
+  slice: newsSlice,
+  actions: newsActions,
+  adapter,
+} = createApiSlice<NewsItem>('news');
+
+export const {
+  get: newsGet,
+  getById: newsGetById,
+  post: newsPost,
+  patch: newsPatch,
+  deleteById: newsDeleteById,
+} = newsActions;
+
+export const {
+  selectIds: newsSelectIds,
+  selectEntities: newsSelectEntities,
+  selectAll: newsSelectAll,
+  selectTotal: newsSelectTotal,
+  selectById: newsSelectById,
+} = adapter.getSelectors(
+  (state: { news: ReturnType<typeof newsSlice['reducer']> }) => state.news,
+);
+
+export const {
+  setFilter: newsSetFilter,
+  dismissError: newsDismissError,
+  clear: newsClear,
+} = newsSlice.actions;
+
+export const newsReducer = newsSlice.reducer;
